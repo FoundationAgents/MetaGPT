03dd73d7292f06246280388f0f69dd1cad31e73c
hiphi730 <2941315986@qq.com>
2026-08-12 17:24:00 +0800

    fix: honor send_to in Team.run_project

--- tests/metagpt/test_team.py
+++ tests/metagpt/test_team.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Desc   : unittest of team
 
+from metagpt.const import MESSAGE_ROUTE_TO_ALL
 from metagpt.roles.project_manager import ProjectManager
 from metagpt.team import Team
 
@@ -11,3 +12,22 @@ def test_team():
     company.hire([ProjectManager()])
 
     assert len(company.env.roles) == 1
+
+
+def test_run_project_send_to():
+    company = Team(use_mgx=False)
+
+    company.run_project("A test idea", send_to="Alex")
+
+    message = company.env.history.get(k=1)[0]
+    assert message.content == "A test idea"
+    assert message.send_to == {"Alex"}
+
+
+def test_run_project_broadcast_by_default():
+    company = Team(use_mgx=False)
+
+    company.run_project("A test idea")
+
+    message = company.env.history.get(k=1)[0]
+    assert message.send_to == {MESSAGE_ROUTE_TO_ALL}
